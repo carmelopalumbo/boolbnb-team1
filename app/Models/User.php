705114a8ba244bebda2f4f979132205b1d661f5a
Jessica <fr_jess@icloud.com>
2023-02-14 16:09:29 +0100

    [add] relaiton user-property

--- app/Models/User.php
+++ app/Models/User.php
@@ -23,6 +23,10 @@ class User extends Authenticatable
         'password',
     ];
 
+    public function properties(){
+        return $this->hasMany(Property::class);
+    }
+
     /**
      * The attributes that should be hidden for serialization.
      *
