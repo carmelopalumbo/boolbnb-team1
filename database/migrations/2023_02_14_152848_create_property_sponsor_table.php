cd2c98964d1de738de363a69fd8ff4ed4b34f5b5
Jessica <fr_jess@icloud.com>
2023-02-14 16:36:00 +0100

    [add] pivot property_sponsor

---
+++ database/migrations/2023_02_14_152848_create_property_sponsor_table.php
@@ -0,0 +1,44 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('property_sponsor', function (Blueprint $table) {
+            $table->unsignedBigInteger('property_id');
+            $table->foreign('property_id')
+                ->references('id')
+                ->on('properties')
+                ->cascadeOnDelete();
+
+            $table->unsignedBigInteger('sponsor_id');
+            $table->foreign('sponsor_id')
+                ->references('id')
+                ->on('sponsors')
+                ->cascadeOnDelete();
+
+            $table->dateTime('start_date');
+            $table->dateTime('end_date');
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('property_sponsor');
+    }
+};
