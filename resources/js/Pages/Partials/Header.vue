053b3222baf08e85244ecf673c45e808abb839b2
Jessica <fr_jess@icloud.com>
2023-02-15 16:27:58 +0100

    [add] font size

--- resources/js/Pages/Partials/Header.vue
+++ resources/js/Pages/Partials/Header.vue
@@ -1,12 +1,15 @@
 <script>
 export default {
+    components: { Link },
 
 }
+import { Link } from '@inertiajs/vue3'
 </script>
 
 <template>
-  <header class="flex justify-center bg-gray-300 h-32 m-0 p-0">
+  <header class="bg-gray-300 h-32 m-0 p-0 flex justify-between content-center">
     <img class="h-40" src="../Admin/Layouts/Images/Logo_V1.png" alt="Logo Boolbnb">
+    <Link href="/admin/stats">Logout</Link>
   </header>
 </template>
 
--- resources/js/Pages/Partials/Nav.vue
+++ resources/js/Pages/Partials/Nav.vue
@@ -1,11 +1,38 @@
 <script>
 export default {
+    components: {Link},
 
 }
+import { Link } from '@inertiajs/vue3'
 </script>
 
 <template>
-    <h1>Hello world</h1>
+      <section class="sidenav w-1/6 bg-slate-600 h-screen m-0">
+
+        <div class="nav">
+
+            <nav class="nav-top p-4">
+                <div class="user rounded-full p-12 bg-red-600 text-center text-2xl">
+                    JT
+                </div>
+                <ul class=" flex flex-col p-0 mt-8 text-lg">
+                    <Link href="/admin">Dashboard</Link>
+                    <Link href="/admin/properties">Appartamenti</Link>
+                    <Link href="/admin/messages">Messaggi</Link>
+                    <Link href="/admin/sponsors">Sponsor</Link>
+                    <Link href="/admin/stats">Statistiche</Link>
+
+                </ul>
+            </nav>
+
+
+        </div>
+
+        <div class="sidenav-bottom">
+
+        </div>
+
+    </section>
 
 </template>
 
