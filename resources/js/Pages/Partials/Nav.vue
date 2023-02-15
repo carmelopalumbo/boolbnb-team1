d0d1255995a58a100ed371b011a296791dc71ba6
Jessica <fr_jess@icloud.com>
2023-02-15 17:21:46 +0100

    [fix] rounded nav

--- resources/js/Pages/Partials/Nav.vue
+++ resources/js/Pages/Partials/Nav.vue
@@ -7,15 +7,17 @@ import { Link } from '@inertiajs/vue3'
 </script>
 
 <template>
-      <section class="sidenav w-1/6 bg-slate-600 h-screen m-0">
+      <section class="sidenav w-1/6 bg-gray-400 h-screen m-0">
 
         <div class="nav">
 
             <nav class="nav-top p-4">
-                <div class="user rounded-full p-12 bg-red-600 text-center text-2xl">
-                    JT
+                <div class="m-8">
+                    <div class="user rounded-full h-32 w-32 flex items-center justify-center bg-cyan-700 text-center text-7xl">
+                        JT
+                    </div>
                 </div>
-                <ul class=" flex flex-col p-0 mt-8 text-lg">
+                <ul class=" flex flex-col p-0 mt-8 text-lg text-white font-bold">
                     <Link href="/admin">Dashboard</Link>
                     <Link href="/admin/properties">Appartamenti</Link>
                     <Link href="/admin/messages">Messaggi</Link>
