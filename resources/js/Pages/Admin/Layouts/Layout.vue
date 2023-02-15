7a043b1c027d0c8aadfa05a0f9dc550c70d7a50e
Jessica <fr_jess@icloud.com>
2023-02-15 16:26:02 +0100

    [to fix] position header

--- resources/js/Pages/Admin/Layouts/Layout.vue
+++ resources/js/Pages/Admin/Layouts/Layout.vue
@@ -1,22 +1,17 @@
 <script>
 export default {
   components: { Nav, Header },
-  props:{
-    name: String,
-  }
 
 }
 import Nav from '@/Pages/Partials/Nav.vue';
 import Header from '@/Pages/Partials/Header.vue';
+
 
 </script>
 
 <template>
     <Header />
 
-    <header>
-        <h1 class="text-blue-600">Prova layout {{ name }}</h1>
-    </header>
     <Nav />
 
     <slot/>
