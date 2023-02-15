5ac4622b499d0bab7672cb782990dbf9ea80d9fb
Jessica <fr_jess@icloud.com>
2023-02-15 17:40:08 +0100

    [change] aside dimensions

--- resources/js/Pages/Partials/Nav.vue
+++ resources/js/Pages/Partials/Nav.vue
@@ -7,19 +7,19 @@ import { Link } from '@inertiajs/vue3'
 </script>
 
 <template>
-      <section class="sidenav w-1/6 bg-gray-400 h-screen m-0">
+      <section class="sidenav w-1/12 bg-gray-400 h-screen m-0">
 
         <div class="nav">
 
             <nav class="nav-top p-4">
-                <div class="m-8">
+                <div class="mx-3">
                     <Link href="/admin">
-                        <div class="user rounded-full h-32 w-32 flex items-center justify-center bg-cyan-700 text-center text-7xl">
+                        <div class="user rounded-full h-16 w-16 flex items-center justify-center bg-cyan-700 text-center text-4xl">
                             JT
                         </div>
                     </Link>
                 </div>
-                <ul class=" flex flex-col p-0 ml-6 mt-8 text-lg text-white font-bold">
+                <ul class=" flex flex-col p-0 ml-1 mt-8 text-lg text-white font-bold">
                     <Link href="/admin/properties">Proprietà</Link>
                     <Link href="/admin/messages">Messaggi</Link>
                     <Link href="/admin/sponsors">Sponsor</Link>
