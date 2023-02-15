696d95f92d7a2cb5ad9b3c53cbc0e69776507df3
Jessica <fr_jess@icloud.com>
2023-02-15 16:25:47 +0100

    [fix] header

--- resources/js/Pages/Partials/Header.vue
+++ resources/js/Pages/Partials/Header.vue
@@ -5,8 +5,8 @@ export default {
 </script>
 
 <template>
-  <header class="flex justify-center bg-gray-300 h-32">
-    <img src="../Admin/Layouts/Images/Logo_V1.png" alt="Logo Boolbnb">
+  <header class="flex justify-center bg-gray-300 h-32 m-0 p-0">
+    <img class="h-40" src="../Admin/Layouts/Images/Logo_V1.png" alt="Logo Boolbnb">
   </header>
 </template>
 
