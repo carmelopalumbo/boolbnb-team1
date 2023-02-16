a0149f7222e10f02bf632f8d6b3bd456864ef2fa
Jessica <fr_jess@icloud.com>
2023-02-16 16:59:26 +0100

    [fix] colors

--- resources/js/Pages/Partials/Footer.vue
+++ resources/js/Pages/Partials/Footer.vue
@@ -39,15 +39,15 @@ export default {
     <section>
 
         <footer class="fixed bottom-0 left-0 z-20 w-full p-4 bg-white border-t border-gray-200 shadow md:flex md:items-center md:justify-between md:p-6 dark:bg-gray-800 dark:border-gray-600">
-            <span class="text-sm text-gray-800 sm:text-center dark:text-gray-400">© 2023 <a href="https://flowbite.com/" class="hover:underline">BoolBnb</a>. All Rights Reserved. |
-                <span class="text-sm text-gray-800 sm:text-center dark:text-gray-400"><a href="#" class="hover:underline">{{ dateTime.hours }}:{{ dateTime.minutes }}:{{ dateTime.seconds }}</a>
+            <span class="text-sm text-gray-800 sm:text-center dark:text-gray-400">© 2023 <a href="https://flowbite.com/" class="hover:text-[#4d1635]">BoolBnb</a>. All Rights Reserved. |
+                <span class="text-sm text-gray-800 sm:text-center dark:text-gray-400"><a href="#" class="hover:text-[#4d1635]">{{ dateTime.hours }}:{{ dateTime.minutes }}:{{ dateTime.seconds }}</a>
                 </span>
             </span>
 
 
             <ul class="flex flex-wrap items-center mt-3 mr-4 text-sm text-gray-800 dark:text-gray-400 sm:mt-0">
                 <li>
-                    <a href="#" class="mr-4 text-base font-bold hover:text-cyan-700 md:mr-6 ">Chi shiamo</a>
+                    <a href="#" class="mr-4 text-base font-bold hover:text-[#4d1635] md:mr-6 ">Chi siamo</a>
                 </li>
             </ul>
 
