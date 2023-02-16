baf76fdc48c3d268b91f00f90b464d02f41f81ba
Jessica <fr_jess@icloud.com>
2023-02-16 19:15:34 +0100

    [fix dimensions]

--- resources/js/Pages/Admin/Edit.vue
+++ resources/js/Pages/Admin/Edit.vue
@@ -30,49 +30,48 @@ export default {
 };
 </script>
 <template>
-    <div class="flex flex-col justify-center mx-auto">
-        <div class="pt-20 inline-block content-center sm:px-6 lg:px-8">
-            <div class="overflow-hidden">
-                <h1 class="text-center font-bold text-2xl py-6 uppercase">
-                    Modifica la tua proprietà
-                </h1>
-            </div>
+    <div class="flex items-center w-full">
+        <div class="pt-20 inline-block content-center w-full">
+            <h1 class="text-center font-bold text-2xl py-6 uppercase">
+                Modifica la tua proprietà
+            </h1>
+
+            <form class="md:flex md:flex-wrap md:justify-between mx-6 md:mx-64" @submit.prevent="submit">
+                <div class="flex flex-col mb-4 md:w-full">
+                <label class="mb-2 text-sm font-medium text-gray-900 dark:text-white">Name</label>
+                <input type="text" id="name" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg block p-2.5" required v-model="propertyEdit.name">
+                </div>
+                <div class="flex flex-col mb-4 md:w-full">
+                <label for="text" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Description</label>
+                <input type="text" id="description" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg block  p-2.5" required v-model="propertyEdit.description">
+                </div>
+
+                <div class="flex flex-col mb-4 md:w-full">
+                <label class="block mb-2 text-sm font-medium text-gray-900 dark:text-white" for="default_size">Cover image</label>
+                <input class="block  text-sm text-gray-900 border border-gray-300 rounded-lg cursor-pointer bg-gray-50" id="file_input" type="file">
+                </div>
+
+                <div class="flex flex-col mb-4 md:w-full">
+                <label for="beds" class="block mb-2  text-sm font-medium text-gray-900 dark:text-white">N° beds</label>
+                <input type="text" id="beds" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg block p-2.5" required v-model="propertyEdit.beds">
+                </div>
+
+                <div class="flex flex-col mb-4 md:w-full">
+                <label for="price" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Price/Night</label>
+                <input type="text" id="price" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg block  p-2.5 " required v-model="propertyEdit.price">
+                </div>
+
+                <div class="flex flex-col mb-4 md:w-full">
+                <label for="address" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Address</label>
+                <input type="text" id="address" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg block  p-2.5" required v-model="propertyEdit.address">
+                </div>
+
+                <button type="submit" class="text-white bg-[#4d1635] hover:bg-[#89275e] font-medium rounded-lg text-sm  sm:w-24 px-5 py-2.5 text-center">Submit</button>
+            </form>
         </div>
     </div>
 
-    <div class="flex flex-col justify-center mx-2 mt-20">
-        <form class="content-center mx-10" @submit.prevent="submit">
-          <div class="mb-6">
-            <label class="mb-2 text-sm font-medium text-gray-900 dark:text-white">Name</label>
-            <input type="text" id="name" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg  block w-1/2 p-2.5" required v-model="propertyEdit.name">
-          </div>
-          <div class="mb-6 mx-auto">
-            <label for="text" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Description</label>
-            <input type="text" id="description" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg  block w-1/2 p-2.5" required v-model="propertyEdit.description">
-          </div>
-
-          <div class="mb-6">
-            <label class="block mb-2 text-sm font-medium text-gray-900 dark:text-white" for="default_size">Cover image</label>
-            <input class="block w-1/2 text-sm text-gray-900 border border-gray-300 rounded-lg cursor-pointer bg-gray-50" id="file_input" type="file">
-          </div>
-
-          <div class="mb-6">
-            <label for="beds" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">N° beds</label>
-            <input type="text" id="beds" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg block w-1/2 p-2.5" required v-model="propertyEdit.beds">
-          </div>
-
-          <div class="mb-6">
-            <label for="price" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Price/Night</label>
-            <input type="text" id="price" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg block w-1/2 p-2.5 " required v-model="propertyEdit.price">
-          </div>
-
-          <div class="mb-6">
-            <label for="address" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Address</label>
-            <input type="text" id="address" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg block w-1/2 p-2.5" required v-model="propertyEdit.address">
-          </div>
-
-          <button type="submit" class="text-white bg-[#4d1635] hover:bg-[#89275e] font-medium rounded-lg text-sm w-1/2 sm:w-1/6 px-5 py-2.5 text-center">Submit</button>
-        </form>
+    <div class="flex flex-col justify-center items-center mt-20 mx-auto w-auto">
 
     </div>
 
