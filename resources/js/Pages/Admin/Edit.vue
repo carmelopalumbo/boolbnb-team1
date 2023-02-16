c3c9545f2a7de858b7b9278d305d2545fbdb3c61
Jessica <fr_jess@icloud.com>
2023-02-16 18:35:28 +0100

    [fix] margin form

--- resources/js/Pages/Admin/Edit.vue
+++ resources/js/Pages/Admin/Edit.vue
@@ -30,169 +30,52 @@ export default {
 };
 </script>
 <template>
-  <div class="container mx-auto h-screen flex">
-    <h1>Create</h1>
+    <div class="flex flex-col justify-center mx-auto">
+        <div class="py-2 inline-block content-center sm:px-6 lg:px-8">
+            <div class="overflow-hidden">
+                <h1 class="text-center font-bold text-2xl py-6 uppercase">
+                    Modifica la tua proprietà
+                </h1>
+            </div>
+        </div>
+    </div>
 
-    <form class="bg-white shadow-md rounded m-auto" @submit.prevent="submit">
-      <div class="mb-4">
-        <label class="block text-gray-700 text-sm font-bold mb-2" for="name"
-          >Name:</label
-        >
+    <div class="flex flex-col justify-center mx-2 mt-20">
+        <form class="content-center mx-10" @submit.prevent="submit">
+          <div class="mb-6">
+            <label class="mb-2 text-sm font-medium text-gray-900 dark:text-white">Name</label>
+            <input type="text" id="name" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg  block w-1/2 p-2.5" required v-model="propertyEdit.name">
+          </div>
+          <div class="mb-6 mx-auto">
+            <label for="text" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Description</label>
+            <input type="text" id="description" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg  block w-1/2 p-2.5" required v-model="propertyEdit.description">
+          </div>
 
-        <input
-          class="
-            shadow
-            appearance-none
-            border
-            rounded
-            w-full
-            py-2
-            px-3
-            text-gray-700
-            leading-tight
-            focus:outline-none focus:shadow-outline
-          "
-          type="text"
-          id="name"
-          v-model="propertyEdit.name"
-        />
-      </div>
+          <div class="mb-6">
+            <label class="block mb-2 text-sm font-medium text-gray-900 dark:text-white" for="default_size">Cover image</label>
+            <input class="block w-1/2 text-sm text-gray-900 border border-gray-300 rounded-lg cursor-pointer bg-gray-50" id="file_input" type="file">
+          </div>
 
-      <div class="mb-4">
-        <label
-          class="block text-gray-700 text-sm font-bold mb-2"
-          for="description"
-          >description:</label
-        >
-        <input
-          class="
-            shadow
-            appearance-none
-            border
-            rounded
-            w-full
-            py-2
-            px-3
-            text-gray-700
-            leading-tight
-            focus:outline-none focus:shadow-outline
-          "
-          type="text"
-          id="description"
-          v-model="propertyEdit.description"
-        />
-      </div>
+          <div class="mb-6">
+            <label for="beds" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">N° beds</label>
+            <input type="text" id="beds" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg block w-1/2 p-2.5" required v-model="propertyEdit.beds">
+          </div>
 
-      <div class="mb-4">
-        <label
-          class="block text-gray-700 text-sm font-bold mb-2"
-          for="cover_image"
-          >cover_image:</label
-        >
-        <input
-          class="
-            shadow
-            appearance-none
-            border
-            rounded
-            w-full
-            py-2
-            px-3
-            text-gray-700
-            leading-tight
-            focus:outline-none focus:shadow-outline
-          "
-          type="text"
-          id="cover_image"
-          v-model="propertyEdit.cover_image"
-        />
-      </div>
+          <div class="mb-6">
+            <label for="price" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Price/Night</label>
+            <input type="text" id="price" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg block w-1/2 p-2.5 " required v-model="propertyEdit.price">
+          </div>
 
-      <div class="mb-4">
-        <label class="block text-gray-700 text-sm font-bold mb-2" for="beds"
-          >beds:</label
-        >
-        <input
-          class="
-            shadow
-            appearance-none
-            border
-            rounded
-            w-full
-            py-2
-            px-3
-            text-gray-700
-            leading-tight
-            focus:outline-none focus:shadow-outline
-          "
-          type="number"
-          id="beds"
-          v-model="propertyEdit.beds"
-        />
-      </div>
+          <div class="mb-6">
+            <label for="address" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Address</label>
+            <input type="text" id="address" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg block w-1/2 p-2.5" required v-model="propertyEdit.address">
+          </div>
 
-      <div class="mb-4">
-        <label class="block text-gray-700 text-sm font-bold mb-2" for="price"
-          >price:</label
-        >
-        <input
-          class="
-            shadow
-            appearance-none
-            border
-            rounded
-            w-full
-            py-2
-            px-3
-            text-gray-700
-            leading-tight
-            focus:outline-none focus:shadow-outline
-          "
-          type="number"
-          id="price"
-          v-model="propertyEdit.price"
-        />
-      </div>
+          <button type="submit" class="text-white bg-[#4d1635] hover:bg-[#89275e] focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-1/2 sm:w-auto px-5 py-2.5 text-center">Submit</button>
+        </form>
 
-      <div class="mb-4">
-        <label class="block text-gray-700 text-sm font-bold mb-2" for="address"
-          >address:</label
-        >
-        <input
-          class="
-            shadow
-            appearance-none
-            border
-            rounded
-            w-full
-            py-2
-            px-3
-            text-gray-700
-            leading-tight
-            focus:outline-none focus:shadow-outline
-          "
-          type="text"
-          id="address"
-          v-model="propertyEdit.address"
-        />
-      </div>
+    </div>
 
-      <button
-        class="
-          bg-blue-500
-          hover:bg-blue-700
-          text-white
-          font-bold
-          py-2
-          px-4
-          rounded
-        "
-        type="submit"
-      >
-        Submit
-      </button>
-    </form>
-  </div>
 </template>
 
 
