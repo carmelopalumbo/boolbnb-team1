e151738df899c216d07ccd3d096c2fadb9106111
Jessica <fr_jess@icloud.com>
2023-02-16 16:44:33 +0100

    [fix] footer header

--- resources/js/Pages/Partials/Footer.vue
+++ resources/js/Pages/Partials/Footer.vue
@@ -1,5 +1,5 @@
 <script>
-import { Footer } from 'flowbite-vue'
+import { Footer } from 'flowbite-vue';
 import { Link } from '@inertiajs/vue3';
 
 const date = new Date();
@@ -37,15 +37,17 @@ export default {
 
 <template>
     <section>
-        <footer class="fixed bottom-0 left-0 z-20 w-full p-4 bg-white border-t border-gray-200 shadow md:flex md:items-center md:justify-between md:p-6 dark:bg-gray-800 dark:border-gray-600">
-            <span class="text-sm text-gray-800 sm:text-center dark:text-gray-400">© 2023 <a href="https://flowbite.com/" class="hover:underline">BoolBnb</a>. All Rights Reserved.
-            </span>
-            <span class="text-lg text-gray-500 sm:text-center dark:text-gray-400"><a href="https://flowbite.com/" class="hover:underline">{{ dateTime.hours }}:{{ dateTime.minutes }}:{{ dateTime.seconds }}</a>
 
+        <footer class="fixed bottom-0 left-0 z-20 w-full p-4 bg-white border-t border-gray-200 shadow md:flex md:items-center md:justify-between md:p-6 dark:bg-gray-800 dark:border-gray-600">
+            <span class="text-sm text-gray-800 sm:text-center dark:text-gray-400">© 2023 <a href="https://flowbite.com/" class="hover:underline">BoolBnb</a>. All Rights Reserved. |
+                <span class="text-sm text-gray-800 sm:text-center dark:text-gray-400"><a href="https://flowbite.com/" class="hover:underline">{{ dateTime.hours }}:{{ dateTime.minutes }}:{{ dateTime.seconds }}</a>
+                </span>
             </span>
-            <ul class="flex flex-wrap items-center mt-3 text-sm text-gray-500 dark:text-gray-400 sm:mt-0">
+
+
+            <ul class="flex flex-wrap items-center mt-3 mr-4 text-sm text-gray-800 dark:text-gray-400 sm:mt-0">
                 <li>
-                    <a href="#" class="mr-4 hover:underline md:mr-6 ">Chi shiamo</a>
+                    <a href="#" class="mr-4 text-base font-bold hover:text-cyan-700 md:mr-6 ">Chi shiamo</a>
                 </li>
             </ul>
 
