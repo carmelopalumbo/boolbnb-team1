0a9759876ab1dcc5ba8c0abaecdf389b686e8cc1
Jessica <fr_jess@icloud.com>
2023-02-16 18:36:39 +0100

    button color

--- resources/js/Pages/Admin/Edit.vue
+++ resources/js/Pages/Admin/Edit.vue
@@ -71,7 +71,7 @@ export default {
             <input type="text" id="address" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg block w-1/2 p-2.5" required v-model="propertyEdit.address">
           </div>
 
-          <button type="submit" class="text-white bg-[#4d1635] hover:bg-[#89275e] focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-1/2 sm:w-auto px-5 py-2.5 text-center">Submit</button>
+          <button type="submit" class="text-white bg-[#4d1635] hover:bg-[#89275e] font-medium rounded-lg text-sm w-1/2 sm:w-1/6 px-5 py-2.5 text-center">Submit</button>
         </form>
 
     </div>
