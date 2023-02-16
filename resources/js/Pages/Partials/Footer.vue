e3041166c1b82f50c1b8959b9369cf6b47e10cb7
Jessica <fr_jess@icloud.com>
2023-02-16 16:50:39 +0100

    [fix] links

--- resources/js/Pages/Partials/Footer.vue
+++ resources/js/Pages/Partials/Footer.vue
@@ -40,7 +40,7 @@ export default {
 
         <footer class="fixed bottom-0 left-0 z-20 w-full p-4 bg-white border-t border-gray-200 shadow md:flex md:items-center md:justify-between md:p-6 dark:bg-gray-800 dark:border-gray-600">
             <span class="text-sm text-gray-800 sm:text-center dark:text-gray-400">© 2023 <a href="https://flowbite.com/" class="hover:underline">BoolBnb</a>. All Rights Reserved. |
-                <span class="text-sm text-gray-800 sm:text-center dark:text-gray-400"><a href="https://flowbite.com/" class="hover:underline">{{ dateTime.hours }}:{{ dateTime.minutes }}:{{ dateTime.seconds }}</a>
+                <span class="text-sm text-gray-800 sm:text-center dark:text-gray-400"><a href="#" class="hover:underline">{{ dateTime.hours }}:{{ dateTime.minutes }}:{{ dateTime.seconds }}</a>
                 </span>
             </span>
 
--- resources/js/Pages/Partials/Header.vue
+++ resources/js/Pages/Partials/Header.vue
@@ -19,7 +19,7 @@ import { Link } from '@inertiajs/vue3';
               <NavbarLink className="text-base text-center "><Link href="/admin/messages" class="align-middle hover:text-cyan-700"><i class="fa-solid fa-message mr-1"></i><span class="">Messaggi</span></Link></NavbarLink>
               <NavbarLink className="text-base text-center"><Link href="/admin/sponsors" class="align-middle hover:text-cyan-700"><i class="fa-solid fa-credit-card mr-1"></i><span class="">Sponsor</span></Link></NavbarLink>
               <NavbarLink className="text-base text-center"><Link href="/admin/stats" class="align-middle hover:text-cyan-700"><i class="fa-solid fa-ranking-star mr-1"></i><span class="">Statistiche</span></Link></NavbarLink>
-              <button type="button" class="text-white bg-cyan-700 focus:ring-4 focus:outline-none focus:ring-blue-300 text-sm px-3 py-2.5 text-center md:mr-0 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800 hover:bg-cyan-900 font-bold rounded-lg"><Link href="/logout" method="post" as="button"></Link>Logout</button>
+              <button type="button" class="text-white bg-cyan-700 focus:ring-4 focus:outline-none focus:ring-blue-300 text-sm px-3 py-2.5 text-center md:mr-0 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800 hover:bg-cyan-900 font-bold rounded-lg w-25"><Link href="/logout" method="post" as="button"></Link>Logout</button>
             </NavbarCollapse>
           </template>
 
