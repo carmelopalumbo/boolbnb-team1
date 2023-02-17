4e47dc2f2e76e3a15155cdda962123959b587fec
Jessica <fr_jess@icloud.com>
2023-02-17 16:12:17 +0100

    v-if style

--- resources/js/Pages/Admin/Index.vue
+++ resources/js/Pages/Admin/Index.vue
@@ -31,7 +31,21 @@ export default {
     </div>
 
     <div class="relative container mx-auto overflow-x-auto">
-        <div class="relative container mx-auto w-5/6">
+
+        <div class="relative container mx-auto w-5/6"
+            v-if="!my_properties.length">
+            <Link
+                href="/admin/properties/create"
+                class="hover:text-[#4d1635] text-lg font-bold uppercase text-green-700 mx-auto inline-block"
+
+            >
+                <i class="fa-solid fa-plus mr-1"></i
+                ><span class="">Aggiungi la tua prima proprietà</span>
+            </Link>
+        </div>
+
+        <div v-else>
+            <div class="relative container mx-auto w-5/6">
             <Link
                 href="/admin/properties/create"
                 class="hover:text-[#4d1635] text-lg font-bold uppercase text-green-700 mx-auto inline-block"
@@ -40,100 +54,95 @@ export default {
                 ><span class="">Aggiungi una proprietà</span></Link
             >
         </div>
+            <table class="w-5/6 text-base mx-auto text-left text-gray-500">
+                <thead
+                    class="border-b text-base text-gray-700 bg-gray-200 uppercase h-10"
+                >
+                    <tr>
+                        <th scope="col" class="font-bold text-gray-900 pl-1">
+                            nome
+                        </th>
+                        <th
+                            scope="col"
+                            class="font-bold text-gray-900 pl-1 hidden md:table-cell"
+                        >
+                            indirizzo
+                        </th>
+                        <th
+                            scope="col"
+                            class="font-bold text-gray-900 pl-1 hidden md:table-cell"
+                        >
+                            prezzo
+                        </th>
+                        <th
+                            scope="col"
+                            class="font-bold text-gray-900 pl-1 hidden md:table-cell"
+                        >
+                            visibilità
+                        </th>
+                        <th scope="col" class="font-bold text-gray-900 pl-1">
+                            azioni
+                        </th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr
+                        v-for="property in my_properties"
+                        :key="property.id"
+                        class="bg-white border-b hover:bg-gray-100 h-16"
+                    >
+                        <td
+                            class="font-medium text-gray-900 whitespace-nowrap break-words"
+                        >
+                            {{ property.name }}
+                        </td>
+                        <td
+                            class="font-normal text-gray-900 whitespace-nowrap hidden md:table-cell"
+                        >
+                            {{ property.address }}
+                        </td>
+                        <td
+                            class="font-normal text-gray-900 whitespace-nowrap hidden md:table-cell"
+                        >
+                            {{ property.price }} €
+                        </td>
+                        <td
+                            class="font-normal text-gray-900 whitespace-nowrap uppercase hidden md:table-cell"
+                        >
+                            {{
+                                property.is_visible
+                                    ? "annuncio pubblico"
+                                    : "annuncio privato"
+                            }}
+                        </td>
+                        <td class="font-normal text-gray-900 whitespace-nowrap">
+                            <Link
+                                :href="route('properties.show', property)"
+                                class="text-white hover:text-black transition delay-150 bg-blue-800 hover:bg-blue-600 px-3 py-2 rounded-lg"
+                            >
+                                <i class="fa-regular fa-eye"></i>
+                            </Link>
 
-        <div
-            v-if="!my_properties.length"
-            class="flex justify-center hover:text-[#4d1635] uppercase"
-        >
-            Aggiungi la tua prima proprietà
+                            <Link
+                                :href="route('properties.edit', property)"
+                                class="text-white hover:text-black transition delay-150 bg-yellow-400 hover:bg-yellow-200 px-3 py-2 rounded-lg mx-2"
+                            >
+                                <i class="fa-solid fa-pen-to-square"></i>
+                            </Link>
+
+                            <button
+                                class="text-white hover:text-black transition delay-150 bg-red-600 hover:bg-red-400 px-3 py-2 rounded-lg"
+                                @click="deleteItem(property)"
+                            >
+                                <i class="fa-solid fa-trash"></i>
+                            </button>
+                        </td>
+                    </tr>
+                </tbody>
+            </table>
+
         </div>
 
-        <table v-else class="w-5/6 text-base mx-auto text-left text-gray-500">
-            <thead
-                class="border-b text-base text-gray-700 bg-gray-200 uppercase h-10"
-            >
-                <tr>
-                    <th scope="col" class="font-bold text-gray-900 pl-1">
-                        nome
-                    </th>
-                    <th
-                        scope="col"
-                        class="font-bold text-gray-900 pl-1 hidden md:table-cell"
-                    >
-                        indirizzo
-                    </th>
-                    <th
-                        scope="col"
-                        class="font-bold text-gray-900 pl-1 hidden md:table-cell"
-                    >
-                        prezzo
-                    </th>
-                    <th
-                        scope="col"
-                        class="font-bold text-gray-900 pl-1 hidden md:table-cell"
-                    >
-                        visibilità
-                    </th>
-                    <th scope="col" class="font-bold text-gray-900 pl-1">
-                        azioni
-                    </th>
-                </tr>
-            </thead>
-            <tbody>
-                <tr
-                    v-for="property in my_properties"
-                    :key="property.id"
-                    class="bg-white border-b hover:bg-gray-100 h-16"
-                >
-                    <td
-                        class="font-medium text-gray-900 whitespace-nowrap break-words"
-                    >
-                        {{ property.name }}
-                    </td>
-                    <td
-                        class="font-normal text-gray-900 whitespace-nowrap hidden md:table-cell"
-                    >
-                        {{ property.address }}
-                    </td>
-                    <td
-                        class="font-normal text-gray-900 whitespace-nowrap hidden md:table-cell"
-                    >
-                        {{ property.price }} €
-                    </td>
-                    <td
-                        class="font-normal text-gray-900 whitespace-nowrap uppercase hidden md:table-cell"
-                    >
-                        {{
-                            property.is_visible
-                                ? "annuncio pubblico"
-                                : "annuncio privato"
-                        }}
-                    </td>
-                    <td class="font-normal text-gray-900 whitespace-nowrap">
-                        <Link
-                            :href="route('properties.show', property)"
-                            class="text-white hover:text-black transition delay-150 bg-blue-800 hover:bg-blue-600 px-3 py-2 rounded-lg"
-                        >
-                            <i class="fa-regular fa-eye"></i>
-                        </Link>
-
-                        <Link
-                            :href="route('properties.edit', property)"
-                            class="text-white hover:text-black transition delay-150 bg-yellow-400 hover:bg-yellow-200 px-3 py-2 rounded-lg mx-2"
-                        >
-                            <i class="fa-solid fa-pen-to-square"></i>
-                        </Link>
-
-                        <button
-                            class="text-white hover:text-black transition delay-150 bg-red-600 hover:bg-red-400 px-3 py-2 rounded-lg"
-                            @click="deleteItem(property)"
-                        >
-                            <i class="fa-solid fa-trash"></i>
-                        </button>
-                    </td>
-                </tr>
-            </tbody>
-        </table>
     </div>
 </template>
 
