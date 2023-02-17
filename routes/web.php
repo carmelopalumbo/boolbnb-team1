6fccf83543dc9ced409b89b23814bf3d99d477a1
carmelopalumbo <carmelopalumbo@outlook.it>
2023-02-17 11:38:13 +0100

    fix-dashboard-route

--- routes/web.php
+++ routes/web.php
@@ -29,10 +29,6 @@ Route::get('/', function () {
     ]);
 });
 
-Route::get('/dashboard', function () {
-    return Inertia::render('Dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
-
 Route::middleware(['auth', 'verified'])
     ->prefix('admin')
     ->group(function () {
