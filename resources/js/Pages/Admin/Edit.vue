5975eec649132573507b505e15265267da940ec1
Jessica <fr_jess@icloud.com>
2023-02-21 12:23:54 +0100

    [fix] method is_visible

--- resources/js/Pages/Admin/Edit.vue
+++ resources/js/Pages/Admin/Edit.vue
@@ -35,6 +35,7 @@ export default {
                 latitude: this.property.latitude,
                 longitude: this.property.longitude,
                 editGallery: this.media_property,
+                is_visible: this.property.is_visible,
             },
         };
     },
@@ -176,9 +177,10 @@ export default {
                     </div>
                     <div class="mb-4">
                         <label class="relative inline-flex items-center cursor-pointer">
-                            <input type="checkbox" value="" class="sr-only peer">
+                            <input type="checkbox" class="sr-only peer" @click="propertyEdit.is_visible = !property.is_visible" :checked="propertyEdit.is_visible">
                             <div class="w-11 h-6 bg-gray-200 rounded-full peer peer-focus:ring-4 peer-focus:ring-[#89275e] peer-checked:after:translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-0.5 after:left-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-gray-600 peer-checked:bg-[#4d1635]"></div>
-                            <span class="ml-3 text-sm font-medium text-gray-900 dark:text-gray-300">Visibile</span>
+                            <span v-if="propertyEdit.is_visible" class="ml-3 text-sm font-bold text-green-600">Annuncio pubblico</span>
+                            <span v-else class="ml-3 text-sm font-bold text-yellow-400">Annuncio privato</span>
                         </label>
                     </div>
 
