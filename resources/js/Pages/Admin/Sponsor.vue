7d6400ee32a77f5b8230a63bdfc7aa1481983665
Eugenio2104 <109421031+Eugenio2104@users.noreply.github.com>
2023-02-23 17:50:29 +0100

    [delete] input submit

--- resources/js/Pages/Admin/Sponsor.vue
+++ resources/js/Pages/Admin/Sponsor.vue
@@ -1,111 +1,122 @@
 <script>
 import Layout from "./Layouts/Layout.vue";
 import { Link, router, useForm } from "@inertiajs/vue3";
-import axios from 'axios';
+import axios from "axios";
 export default {
-    name: "Sponsor",
-    layout: Layout,
+  name: "Sponsor",
+  layout: Layout,
 
-        token: '',
-        sponsor: '',
-    data(){
-        return{
-        }
+  token: "",
+  sponsor: "",
+  data() {
+    return {};
+  },
+  methods: {
+    async getPayment() {
+      const response = await axios
+        .post(
+          "http://127.0.0.1:8000/payment/make/payment"
+          //vediamo quello che torna nel res
+        )
+        .then((res) => {
+          console.log(res);
+        });
     },
-    methods:{
-
-        async getPayment(){
-            const response = await axios.post(
-                'http://127.0.0.1:8000/payment/make/payment'
-                //vediamo quello che torna nel res
-            ).then((res)=>{
-                console.log(res);
-            })
-        },
-        async generateToken(){
-            const response = await axios.get(
-                'http://127.0.0.1:8000/api/payment/generate'
-            )
-            .then((res) =>{
-                this.token = res.data.clientToken
-                console.log(this.token);
-            })
-        },
-        getPayment(){
-            const form = document.getElementById('payment-form');
-
-            braintree.dropin.create({
-            // Step three: get client token from your server, such as via
-            //    templates or async http request
-            authorization: this.token,
-            container: '#dropin-container'
-            }).then((dropinInstance) => {
-                console.log('entrati in dropinstance');
-                form.addEventListener('submit',(event)=>{
-                    event.preventDefault();
-
-                    console.log('cliccato dentro drop instance');
-
-                    dropinInstance.requestPaymentMethod().then((payload)=>{
-                        console.log('DENTRO METODO PAYMENT');
-                        console.log(payload);
-                        // document.getElementById('nonce').value = payload.nonce;
-                        form.submit();
-                    }).catch((error)=>{throw error});
-                });
-            // Use `dropinInstance` here
-            // Methods documented at https://braintree.github.io/braintree-web-drop-in/docs/current/Dropin.html
-            }).catch((error) => { console.log(error);});
-        },
-         submit(){
-            router.post('/api/payment/make/payment', useForm);
-            console.log('submitted');
-        },
+    async generateToken() {
+      const response = await axios
+        .get("http://127.0.0.1:8000/api/payment/generate")
+        .then((res) => {
+          this.token = res.data.clientToken;
+          console.log(this.token);
+        });
     },
-    mounted(){
-        this.generateToken();
+    getPayment() {
+      const form = document.getElementById("payment-form");
 
-    }
+      braintree.dropin
+        .create({
+          // Step three: get client token from your server, such as via
+          //    templates or async http request
+          authorization: this.token,
+          container: "#dropin-container",
+        })
+        .then((dropinInstance) => {
+          console.log("entrati in dropinstance");
+          form.addEventListener("submit", (event) => {
+            event.preventDefault();
+
+            console.log("cliccato dentro drop instance");
+
+            dropinInstance
+              .requestPaymentMethod()
+              .then((payload) => {
+                console.log("DENTRO METODO PAYMENT");
+                console.log(payload);
+                // document.getElementById('nonce').value = payload.nonce;
+                form.submit();
+              })
+              .catch((error) => {
+                throw error;
+              });
+          });
+          // Use `dropinInstance` here
+          // Methods documented at https://braintree.github.io/braintree-web-drop-in/docs/current/Dropin.html
+        })
+        .catch((error) => {
+          console.log(error);
+        });
+    },
+    submit() {
+      router.post("/api/payment/make/payment", useForm);
+      console.log("submitted");
+    },
+  },
+  mounted() {
+    this.generateToken();
+  },
 };
 </script>
 
 <template>
-    <Head title="BOOST"></Head>
-    <div class="flex justify-center mx-auto">
-        <div class="py-6 inline-block min-w-full sm:px-6 lg:px-8">
-            <div class="overflow-hidden">
-                <h1 class="text-center font-bold text-2xl py-6 uppercase">
-                    BENVENUTO IN BOOST
-                </h1>
-                <p>
-                    In questa sezione hai la possibilitá di mettere in risalto
-                    la tua proprietá. Abbiamo messo a disposizione per te tre
-                    pacchetti. Acquistando uno di questi otterrai un badge e la
-                    tua proprietá sará in cima alle ricerche degli utenti!
-                    Scegli il pacchetto che preferisci e procedi con il
-                    pagamento.
-                </p>
-            </div>
+  <Head title="BOOST"></Head>
+  <div class="flex justify-center mx-auto">
+    <div class="py-6 inline-block min-w-full sm:px-6 lg:px-8">
+      <div class="overflow-hidden">
+        <h1 class="text-center font-bold text-2xl py-6 uppercase">
+          BENVENUTO IN BOOST
+        </h1>
+        <p>
+          In questa sezione hai la possibilitá di mettere in risalto la tua
+          proprietá. Abbiamo messo a disposizione per te tre pacchetti.
+          Acquistando uno di questi otterrai un badge e la tua proprietá sará in
+          cima alle ricerche degli utenti! Scegli il pacchetto che preferisci e
+          procedi con il pagamento.
+        </p>
+      </div>
 
-<form  id="payment-form" action="/api/payment/make/payment" method="post" @submit.prevent="submit">
-
+      <form
+        id="payment-form"
+        action="/api/payment/make/payment"
+        method="post"
+        @submit.prevent="submit"
+      >
         <div id="dropin-wrapper">
-            <div id="checkout-message"></div>
-            <div id="dropin-container"></div>
+          <div id="checkout-message"></div>
+          <div id="dropin-container"></div>
 
+          <button
+            class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+            id="submit-button"
+            @click="getPayment"
+          >
+            Submit payment
+          </button>
 
-            <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded" id="submit-button" @click="getPayment" >Submit payment</button>
-
-             <input type="submit" />
-             <input type="hidden" id="nonce" name="payment_method_nonce"/>
+          <input type="hidden" id="nonce" name="payment_method_nonce" />
         </div>
-
-</form>
-
-
-
-        </div>
+      </form>
     </div>
+  </div>
 </template>
 
 <style>
