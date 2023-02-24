5ca3ee08d7cdf6d122e37a33ffe52ae95bac3866
Jessica <fr_jess@icloud.com>
2023-02-24 12:27:06 +0100

    [fix] opacity

--- resources/js/Pages/Partials/PropertyCard.vue
+++ resources/js/Pages/Partials/PropertyCard.vue
@@ -9,19 +9,16 @@ export default {
 
 <template>
     <div class="group container max-w-sm">
-        <div
-            class="max-w-sm border-2 rounded-xl border-[#4d1635] m-5 relative flex align-items-center justify-content-center"
-        >
-            <!-- <img class="rounded-xl object-none h-48 w-96 group-hover:opacity-25 opacity-60" src="https://images.unsplash.com/photo-1522093007474-d86e9bf7ba6f?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=400&q=80" alt=""/> -->
-
+        <div class="max-w-sm border-2 rounded-xl border-[#4d1635] m-5 relative flex align-items-center justify-content-center">
             <img
                 class="rounded-xl bg-cover bg-center bg-no-repeat h-48 w-96 group-hover:opacity-25 opacity-30"
                 :src="
                     'http://[::1]:5173/storage/app/public/' +
                     property.cover_image
                 "
-                alt=""
+                alt="Cover image"
             />
+
 
             <div
                 class="p-5 group-hover:opacity-0 absolute description text-[#89275e] font-bold"
