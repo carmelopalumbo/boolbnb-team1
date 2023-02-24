9a01f3c2904c91925d55330c7bcbdc78cb7fe3ca
Jessica <fr_jess@icloud.com>
2023-02-24 11:22:01 +0100

    [edit] home search

--- resources/js/Pages/Guest/Home.vue
+++ resources/js/Pages/Guest/Home.vue
@@ -119,107 +119,77 @@ export default {
 
     <h1 class="text-center pt-6 uppercase">Sito pubblico</h1>
 
-    <div class="flex justify-center">
+    <div class="flex justify-center mt-6">
         <div class="flex flex-col mb-4">
-                    <input
-                     @keyup="debounced"
-                        type="text"
-                        class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg block p-2.5 w-56"
-                        v-model="search"
-                        placeholder="Cerca un indirizzo . . . "
-                    />
-                    <div v-if="listAddress.length" class="listAddress">
-                        <p class="p-3" v-for="item in listAddress" :key="item">
-                            <ul>
-                                <li class="selectAddress cursor-pointer"
-                                @click="selectAddress(item)"
+            <input
+                @keyup="debounced"
+                type="text"
+                class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg block p-2.5 w-full"
+                v-model="search"
+                placeholder="Cerca un indirizzo . . . "
+            />
+            <div v-if="listAddress.length" class="listAddress">
+                <p class="p-3" v-for="item in listAddress" :key="item">
+                    <ul>
+                        <li class="selectAddress cursor-pointer"
+                        @click="selectAddress(item)"
 
-                                >{{item.address.freeformAddress}}</li>
-                            </ul>
-                        </p>
-                    </div>
-                </div>
-
-        <button
-            id="dropdownMenuIconButton"
-            data-dropdown-toggle="dropdownDots"
-            class="inline-flex items-center ml-2 p-2 text-sm font-medium text-center text-gray-900 bg-white rounded-lg hover:bg-gray-100 focus:ring-4 focus:outline-none dark:text-white focus:ring-gray-50 dark:bg-gray-800 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
-            type="button"
-        >
-            <svg
-                class="w-6 h-6"
-                aria-hidden="true"
-                fill="currentColor"
-                viewBox="0 0 20 20"
-                xmlns="http://www.w3.org/2000/svg"
-            >
-                <path
-                    d="M10 6a2 2 0 110-4 2 2 0 010 4zM10 12a2 2 0 110-4 2 2 0 010 4zM10 18a2 2 0 110-4 2 2 0 010 4z"
-                ></path>
-            </svg>
-        </button>
-
-        <!-- Dropdown menu -->
-        <div
-            id="dropdownDots"
-            class="z-10 hidden bg-white divide-y divide-gray-100 rounded-lg shadow w-5/6 dark:bg-gray-700 dark:divide-gray-600"
-        >
+                        >{{item.address.freeformAddress}}</li>
+                    </ul>
+                </p>
+            </div>
             <form action="">
-                <ul
-                    class="py-2 text-sm text-gray-700 dark:text-gray-200"
+                <div
+                    class="py-2 text-sm text-gray-700 dark:text-gray-200 flex justify-between gap-4"
                     aria-labelledby="dropdownMenuIconButton"
                 >
-                    <li>
+                <div class="w-1/2">
+                    <label for="services" class="block mb-2 text-sm font-medium text-gray-900">N° posti letti</label>
                         <input
                             v-model="beds"
                             min="0"
                             type="number"
-                            class="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white"
+                            class="block px-4 py-2 hover:bg-gray-100 border border-gray-200 rounded-lg w-full"
                             placeholder="numero letti"
                         />
-                    </li>
-                    <li>
-                        <input
-                            v-model="rooms"
-                            min="0"
-                            type="number"
-                            class="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white"
-                            placeholder="numero stanze"
-                        />
-                    </li>
-                </ul>
-                <button
-                    @click.prevent="submit"
-                    :disabled="!search.length"
-                    type="button"
-                    class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
-                >
-                    Submit
-                </button>
+
+                </div>
+
+                <div class="w-1/2">
+                    <label for="services" class="block mb-2 text-sm font-medium text-gray-900">N° bagni</label>
+
+                    <input
+                        v-model="rooms"
+                        min="0"
+                        type="number"
+                        class="block px-4 py-2 hover:bg-gray-100 border border-gray-200 rounded-lg w-full"
+                        placeholder="numero stanze"
+                    />
+                </div>
+
+                </div>
             </form>
 
-            <h3 class="mb-4 font-semibold text-gray-900 dark:text-white">
-                Servizi
-            </h3>
+            <label for="services" class="block mb-2 text-sm font-medium text-gray-900">Servizi richiesti</label>
             <ul
-                class="items-center w-full text-sm font-medium text-gray-900 bg-white border border-gray-200 rounded-lg sm:flex dark:bg-gray-700 dark:border-gray-600 dark:text-white"
+                class="lg:grid lg:grid-cols-6 place-items-center auto-cols-max align-middle w-full text-sm font-medium text-gray-900 bg-white border border-gray-200 rounded-lg flex flex-wrap p-2"
             >
                 <li
                     v-for="service in services"
                     :key="service.id"
-                    class="w-full border-b border-gray-200 sm:border-b-0 sm:border-r dark:border-gray-600"
+                    class="w-full mb-2 place-self-center"
                 >
-                    <div class="flex items-center pl-3">
+                    <div class="flex items-center">
                         <input
                             v-model="filterServices"
                             :id="service.id"
                             type="checkbox"
                             :value="service.id"
-                            class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-700 dark:focus:ring-offset-gray-700 focus:ring-2 dark:bg-gray-600 dark:border-gray-500"
+                            class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded"
                         />
                         <label
                             :for="service.id"
-                            class="w-full py-3 ml-2 text-sm font-medium text-gray-900 dark:text-gray-300"
+                            class="ml-2 text-sm font-medium text-gray-900"
                             >{{ service.name }}</label
                         >
                     </div>
@@ -233,6 +203,14 @@ export default {
                 <option value="30">30</option>
                 <option value="40">40</option>
             </select>
+            <button
+                @click.prevent="submit"
+                :disabled="!search.length"
+                type="button"
+                class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800 mt-4"
+            >
+                Submit
+            </button>
         </div>
     </div>
 
