ecce26c8644a4ba73dcda7c9acb52690dd7f914e
mikzanco <mikyzancolo97@gmail.com>
2023-02-27 17:50:01 +0100

    fix

--- resources/js/Pages/Guest/About.vue
+++ resources/js/Pages/Guest/About.vue
@@ -15,9 +15,10 @@ export default {
             <div class="flex justify-center m-9 text-3xl uppercase hover:text-[#4d1635]">
                 <i class="fa-solid fa-people-group"></i><h1 class="px-4 font-bold">Chi siamo</h1><i class="fa-solid fa-people-group"></i>
             </div>
-            <div class="mx-auto space-x-6 h-5/6 w-5/6 mt-3 flex justify-between ">
+            <div class="flex flex-col">
+                <div class="mx-auto space-x-6 h-5/6 w-5/6 mt-3 flex md:flex-row flex-col columns-3 justify-between ">
 
-                <div class="w-1/5  hover:scale-105 hover:drop-shadow-xl transition duration-300 rounded-md border-[#4d1635] border-2 min-w-min">
+                <div class=" w-1/2 hover:scale-105 transition duration-300 rounded-md border-[#4d1635] border-2 ">
                     <div class="rounded-full scale-75 border-[#4d1635] border-2 ">
                         <img class="rounded-full " src="https://media.licdn.com/dms/image/D4D03AQHtoJEa__BfSA/profile-displayphoto-shrink_800_800/0/1675531887713?e=1683158400&v=beta&t=GozYAPM7HLLUg-BQWeiswYEQXDK8uxajGvirO83A2UU" alt="">
                     </div>
@@ -30,7 +31,7 @@ export default {
                         <a target="_blank" href="https://github.com/FRJess"><i class="text-2xl fa-brands fa-github"></i> FRJess </a>
                     </div>
                 </div>
-                <div class="w-1/5  hover:scale-105 transition duration-300 rounded-md border-[#4d1635] border-2">
+                <div class=" w-1/2 hover:scale-105 transition duration-300 rounded-md border-[#4d1635] border-2">
                     <div class="rounded-full scale-75 border-[#4d1635] border-2 ">
                         <img class="rounded-full " src="https://media.licdn.com/dms/image/D4D03AQFfbqU0LQuYnw/profile-displayphoto-shrink_800_800/0/1677487021905?e=1683158400&v=beta&t=1b3eq1j18F-d9u_a90Ny2GwVU-zkdg7MIdjcpBkBAP4" alt="">
                     </div>
@@ -38,12 +39,12 @@ export default {
                         <p>Michele Zancolò</p>
                     </div>
                     <div class="text-lg m-5 p-3 text-[#4d1635]">
-                        <a class="pb-8" target="_blank" href="https://www.linkedin.com/in/michele-zancol%C3%B2/"><i class="text-2xl fa-brands fa-linkedin"></i> Visita il profilo </a>
+                        <a target="_blank" href="https://www.linkedin.com/in/michele-zancol%C3%B2/"><i class="text-2xl fa-brands fa-linkedin"></i> Visita il profilo </a>
                         <br>
                         <a target="_blank" href="https://github.com/mikzanco"><i class="text-2xl fa-brands fa-github"></i> mikzanco </a>
                     </div>
                 </div>
-                <div class="w-1/5  hover:scale-105 transition duration-300 rounded-md border-[#4d1635] border-2">
+                <div class=" w-1/2 hover:scale-105 transition duration-300 rounded-md border-[#4d1635] border-2">
                     <div class="rounded-full scale-75 border-[#4d1635] border-2 ">
                         <img class="rounded-full" src="https://media.licdn.com/dms/image/D4E03AQG6Rd-HaTSySg/profile-displayphoto-shrink_800_800/0/1675518344230?e=1683158400&v=beta&t=cJDyPf7CT019VpgXbO2JmAJcKIq5WF66pPy-nJjtibs" alt="">
                     </div>
@@ -56,7 +57,7 @@ export default {
                         <a target="_blank" href="https://github.com/carmelopalumbo"><i class="text-2xl fa-brands fa-github"></i> PALUC </a>
                     </div>
                 </div>
-                <div class="w-1/5  hover:scale-105 transition duration-300 rounded-md border-[#4d1635] border-2">
+                <div class=" w-1/2 hover:scale-105 transition duration-300 rounded-md border-[#4d1635] border-2">
                     <div class="rounded-full scale-75 border-[#4d1635] border-2 ">
                         <img class="rounded-full" src="https://media.licdn.com/dms/image/D4D03AQGGJmXBOgcVjQ/profile-displayphoto-shrink_800_800/0/1677249743869?e=1683158400&v=beta&t=eC_iyuzZ2dfQW_PWPm42Led0IsvlcySeBUk48yLeVIs" alt="">
                     </div>
@@ -69,7 +70,7 @@ export default {
                         <a target="_blank" href="https://github.com/Eugenio2104"><i class="text-2xl fa-brands fa-github"></i> Eugenio2104 </a>
                     </div>
                 </div>
-                <div class="w-1/5  hover:scale-105 transition duration-300 rounded-md border-[#4d1635] border-2">
+                <div class=" w-1/2 hover:scale-105 transition duration-300 rounded-md border-[#4d1635] border-2">
                     <div class="rounded-full scale-75 border-[#4d1635] border-2 ">
                         <img class="rounded-full" src="https://media.licdn.com/dms/image/D4D03AQF7pBADbgRbIQ/profile-displayphoto-shrink_800_800/0/1675442747948?e=1683158400&v=beta&t=4hGCoj-IgKCDhuxOyd1LeR020YNZLrAcqwb0P7sMBLs" alt="">
                     </div>
@@ -84,18 +85,20 @@ export default {
 
                 </div>
 
-            </div>
+                </div>
 
-            <div class="mx-auto w-5/6 mt-7 rounded-md border-[#4d1635] border-2 my-4">
+                <div class="mx-auto w-5/6 mt-7 rounded-md border-[#4d1635] border-2 my-4">
 
                 <p class="p-3">Siamo lieti di esprimere la nostra gratitudine nei confronti di Boolean Careers per l'opportunità che ci hanno offerto di partecipare al loro corso di sviluppo web. Grazie alla loro formazione, supporto e guida, siamo riusciti a realizzare il nostro progetto BoolBnB, una replica di AirBnb. <br> <br>
                 Il corso di sviluppo web che abbiamo frequentato con Boolean Careers è stato un'esperienza incredibile, sia per la qualità del programma di studio che per l'efficienza dei loro docenti e tutor. Siamo rimasti particolarmente colpiti dalla loro professionalità, competenza e disponibilità nell'aiutarci ad acquisire le competenze tecniche necessarie per costruire il nostro sito web. <br> <br>
                 Il nostro team desidera ringraziare tutti i docenti e i tutor che hanno lavorato con noi durante il corso di sviluppo web. Siamo grati per il loro impegno, pazienza e dedizione nell'aiutarci ad affrontare le difficoltà e superare gli ostacoli che abbiamo incontrato lungo il percorso. <br> <br>
                 Ovunque arriveremo in futuro, non scorderemo mai da dove siamo partiti. <br> <br>
                 Il team BoolBnB &hearts; .
-            </p>
+                </p>
 
+                </div>
             </div>
+
 
         </main>
 
@@ -110,12 +113,5 @@ export default {
     /* margin-bottom: 20px; */
 }
 
-.custom-shadow {
-  box-shadow: 0 0 0 3px rgba(77, 22, 53, 0.5);
-}
-
-.custom-shadow:hover {
-  transform: scale(1.05);
-}
 
 </style>
