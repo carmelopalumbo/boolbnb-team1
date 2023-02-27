a1da4177be4738150bf94a13768cac91aff894cd
Carmelo Palumbo <carmelopalumbo@outlook.it>
2023-02-27 10:40:55 +0100

    add-thanks-about-us

--- resources/js/Pages/Guest/About.vue
+++ resources/js/Pages/Guest/About.vue
@@ -86,9 +86,14 @@ export default {
 
             </div>
 
-            <div class="mx-auto  w-5/6 mt-7 rounded-md border-[#4d1635] border-2">
+            <div class="mx-auto w-5/6 mt-7 rounded-md border-[#4d1635] border-2 my-4">
 
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Rerum itaque molestiae tempore beatae dolore! Ut excepturi rem autem, repellat consequuntur quia, illo at illum inventore architecto molestias! Totam doloremque architecto unde eveniet dolore incidunt explicabo hic libero, id quidem. Alias sequi odio quam similique fuga reprehenderit quibusdam debitis culpa vitae maxime fugit amet odit praesentium, accusamus enim officiis non aperiam rem at repellendus? Qui cum quod, ipsam incidunt at facilis, delectus nobis recusandae quis aspernatur dolorum fugit tempore iure maiores voluptatem repudiandae dolores totam saepe voluptatibus, voluptates earum molestias. Quisquam obcaecati quia quae tempora tempore atque minus asperiores veniam, architecto laboriosam aliquid laudantium aliquam voluptate facilis in optio eius eos rem? Esse eligendi animi officiis facere dolores suscipit. Praesentium distinctio consectetur itaque? Consequuntur laborum, a numquam vero delectus voluptatem soluta! Accusamus soluta exercitationem odio quidem perferendis recusandae perspiciatis itaque, nam quos facilis porro accusantium nemo modi excepturi quam molestiae quod sit deleniti at eaque! Eligendi enim, reiciendis dolor aperiam suscipit alias totam dolorum, cupiditate amet temporibus explicabo placeat voluptas vero eveniet hic, deserunt eum deleniti facilis ratione mollitia porro et. Quibusdam laboriosam provident perspiciatis perferendis numquam a! Sapiente, optio exercitationem nemo laudantium voluptatum fuga neque nostrum, nesciunt, eos fugiat veritatis.</p>
+                <p class="p-3">Siamo lieti di esprimere la nostra gratitudine nei confronti di Boolean Careers per l'opportunità che ci hanno offerto di partecipare al loro corso di sviluppo web. Grazie alla loro formazione, supporto e guida, siamo riusciti a realizzare il nostro progetto BoolBnB, una replica di AirBnb. <br> <br>
+                Il corso di sviluppo web che abbiamo frequentato con Boolean Careers è stato un'esperienza incredibile, sia per la qualità del programma di studio che per l'efficienza dei loro docenti e tutor. Siamo rimasti particolarmente colpiti dalla loro professionalità, competenza e disponibilità nell'aiutarci ad acquisire le competenze tecniche necessarie per costruire il nostro sito web. <br> <br>
+                Il nostro team desidera ringraziare tutti i docenti e i tutor che hanno lavorato con noi durante il corso di sviluppo web. Siamo grati per il loro impegno, pazienza e dedizione nell'aiutarci ad affrontare le difficoltà e superare gli ostacoli che abbiamo incontrato lungo il percorso. <br> <br>
+                Ovunque arriveremo in futuro, non scorderemo mai da dove siamo partiti. <br> <br>
+                Il team BoolBnB &hearts; .
+            </p>
 
             </div>
 
